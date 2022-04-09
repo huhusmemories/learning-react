0670ac5990d47447ab775e0d6e60a1ea6f1ac219
Huang Shewei <huangshewei@gmail.com>
2022-04-09 03:08:28 -0400

    Deleting specifics items using ids

--- chapter-3/first-react-app/my-first-react-app/src/App.js
+++ chapter-3/first-react-app/my-first-react-app/src/App.js
@@ -2,16 +2,28 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id:1},
+    {title: "bowser's live stream", id:2},
+    {title: "race on moo moo farm", id:3}
+  ])
 
-  const handleClick = () => {
-    setName('luigi')
-    console.log(name)
+  const handleClick = (id) => {
+    setEvents(events.filter((event)=> {
+      return id !== event.id
+    }))
+    console.log(id)
+
   }
+
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index + 1} - {event.title.toUpperCase()}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete Event</button>
+        </div>
+      ))}
     </div>
   );
 }
