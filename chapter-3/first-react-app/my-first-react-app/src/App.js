c0c3c2d188eada43ee5ebca49e864611ff2c5199
Huang Shewei <huangshewei@gmail.com>
2022-04-11 18:11:51 -0400

    Reusable Event List Component

--- chapter-3/first-react-app/my-first-react-app/src/App.js
+++ chapter-3/first-react-app/my-first-react-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Title from './components/Title'
 import Modal from './components/Modal'
+import EventList from './components/EventList'
 
 function App() {
   const [showModal, setShowModal] = useState(false)
@@ -46,12 +47,7 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title.toUpperCase()}</h2>
-          <button onClick={() => handleClick(event.id)}>Delete Event</button>
-        </React.Fragment>
-      ))}
+      {showEvents && <EventList events={events} handleClick={handleClick}/>}
 
 
         <div>
