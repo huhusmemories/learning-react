ccc84130a3a61ebaf57021b257c427daf329ee6f
Huang Shewei <huangshewei@gmail.com>
2022-04-11 14:46:17 -0400

    Created title prop and some basic style

---
+++ chapter-3/first-react-app/my-first-react-app/src/components/Title.js
@@ -0,0 +1,10 @@
+export default function Title () {
+  return (
+    <div>
+      <h1 className="title"> Mario Kingdom Events</h1>
+      <br />
+      <h2 className="subtitle">All the latest events in Mario Kingdom</h2>
+
+    </div>
+  )
+}
