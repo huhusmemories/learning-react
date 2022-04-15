4c7fdfd42465f33f2877de3cd430cfd8e3007b4b
Huang Shewei <huangshewei@gmail.com>
2022-04-15 16:47:28 -0400

    useEffect for infinite loops

--- chapter-8/tripago/src/hooks/useFetch.js
+++ chapter-8/tripago/src/hooks/useFetch.js
@@ -1,13 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-export const useFetch = (url) => {
+export const useFetch = (url, _options) => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
 
+  // use useRef to wrap an object/array argument
+  // which is a useEffect dependency
+  const options = useRef(_options).current
 
   useEffect(() => {
+    console.log(options)
     const controller = new AbortController()
+
     const fetchData = async () => {
       setIsPending(true)
 
@@ -27,7 +32,6 @@ export const useFetch = (url) => {
         } else {
           setIsPending(false)
           setError("Could not fetch data")
-          console.log(err.message)
         }
       }
 
@@ -38,7 +42,7 @@ export const useFetch = (url) => {
     return () => {
       controller.abort()
     }
-  }, [url])
+  }, [url, options])
 
   return { data, isPending, error }
 }
