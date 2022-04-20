417d1369ef093fe6d54ecb0eb0a05c0f273d8be6
Huang Shewei <huangshewei@gmail.com>
2022-04-19 21:33:40 -0400

    NavLinks and exact match

--- chapter-10/multi-page/multipage-site/src/App.js
+++ chapter-10/multi-page/multipage-site/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 
 // Home components
 
@@ -12,6 +12,9 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
         </nav>
 
         <Switch>
