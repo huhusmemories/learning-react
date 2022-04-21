cb8a171e1b0ccf8711df348568ad5e2e132b08fc
Huang Shewei <huangshewei@gmail.com>
2022-04-21 17:19:50 -0400

    Created navbar component

---
+++ chapter-11/project/cooking-ninja/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// Style
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <Link to="/" className='brand'>
+          <h1>Cooking Ninja</h1>
+        </Link>
+
+        <Link to="/create">
+          Create Recipe
+        </Link>
+      </nav>
+    </div>
+  )
+}
