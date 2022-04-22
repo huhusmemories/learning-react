425df1a2be3b7c3b986bca5c4c538c7462651696
Huang Shewei <huangshewei@gmail.com>
2022-04-22 18:38:29 -0400

    redirecting user

--- chapter-11/project/cooking-ninja/src/pages/create/Create.js
+++ chapter-11/project/cooking-ninja/src/pages/create/Create.js
@@ -1,5 +1,6 @@
-import { React, useState, useRef } from 'react'
+import { React, useState, useRef, useEffect } from 'react'
 import { useFetch } from '../../hooks/useFetch'
+import { useHistory } from 'react-router-dom'
 
 // Style
 import './Create.css'
@@ -11,8 +12,10 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const history = useHistory()
 
   const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -29,6 +32,14 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
+
+  // Redirect user
+  useEffect(() => {
+    if (data) {
+      history.push('/')
+    }
+  }, [data])
+
   return (
     <div className='create'>
       <h2 className='page-title'>Add a new recipe</h2>
