59cc40eb1fec7fc0bbdab30c6fd0e8cef3d757ee
Huang Shewei <huangshewei@gmail.com>
2022-04-22 16:43:48 -0400

    Fetching a single recipe

--- chapter-11/project/cooking-ninja/src/App.js
+++ chapter-11/project/cooking-ninja/src/App.js
@@ -29,7 +29,7 @@ function App() {
             <Search/>
           </Route>
 
-          <Route path="/recipe/:id">
+          <Route path="/recipes/:id">
             <Recipe/>
           </Route>
 
--- chapter-11/project/cooking-ninja/src/pages/recipe/Recipe.js
+++ chapter-11/project/cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
 // Style
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/'+ id
+  const { error, isPending, data: recipe } = useFetch(url)
+  console.log(url)
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
   )
 }
