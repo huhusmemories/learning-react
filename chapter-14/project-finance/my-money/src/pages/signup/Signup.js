45244e104f89c8ab958ccae91a8e8c9e2e228fe0
Huang Shewei <huangshewei@gmail.com>
2022-04-27 17:18:31 -0400

    Created signup page

--- chapter-14/project-finance/my-money/src/pages/signup/Signup.js
+++ chapter-14/project-finance/my-money/src/pages/signup/Signup.js
@@ -1,10 +1,44 @@
 import styles from './Signup.module.css'
-import React from 'react'
+import { useState } from 'react'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName)
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Sign up</h2>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+          <span>password:</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+      </label>
+      <button className='btn'>Sign Up</button>
+
+    </form>
   )
 }
