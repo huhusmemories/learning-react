a38a33fd2e759ceaf9f0387b610f4fcb510d5e88
Huang Shewei <huangshewei@gmail.com>
2022-04-27 16:14:55 -0400

    Navbar created

--- chapter-14/project-finance/my-money/src/App.js
+++ chapter-14/project-finance/my-money/src/App.js
@@ -2,24 +2,27 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // Page Components
 import Home from './pages/home/Home'
-import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
+
           <Route exact path="/">
             <Home/>
           </Route>
 
-          <Route path='/signup'>
-            <Signup/>
-          </Route>
-
           <Route path='/login'>
             <Login/>
+          </Route>
+
+          <Route path='/signup'>
+            <Signup/>
           </Route>
 
         </Switch>
