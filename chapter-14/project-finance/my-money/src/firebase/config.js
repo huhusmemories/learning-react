1d2959af0efe8ebbba23abd16b166695fe607c2c
Huang Shewei <huangshewei@gmail.com>
2022-04-28 13:36:06 -0400

    Setting up firebase and firestore

---
+++ chapter-14/project-finance/my-money/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgeWqMIoh6adyM_fStjqEMLCImNJjKITM",
+  authDomain: "mymoney-8f763.firebaseapp.com",
+  projectId: "mymoney-8f763",
+  storageBucket: "mymoney-8f763.appspot.com",
+  messagingSenderId: "449270227462",
+  appId: "1:449270227462:web:4ba726f1bbebfe83fb5987"
+};
+
+// Init firebase
+firebase.initializeApp(firebaseConfig)
+
+// Init service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
