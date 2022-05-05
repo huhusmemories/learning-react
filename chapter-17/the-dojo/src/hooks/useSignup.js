888bf00e3f72b722e69845ee3e0162468381f40c
Huang Shewei <huangshewei@gmail.com>
2022-05-05 15:10:21 -0400

    Creating user document

--- chapter-17/the-dojo/src/hooks/useSignup.js
+++ chapter-17/the-dojo/src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth, projectStorage } from '../firebase/config'
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -25,9 +25,15 @@ export const useSignup = () => {
       const img = await projectStorage.ref(uploadPath).put(thumbnail)
       const imgUrl = await img.ref.getDownloadURL()
 
-
       // add display name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl })
+
+      // create a user doc
+      await projectFirestore.collection('users').doc(res.user.uid).set({
+        online: true,
+        displayName,
+        photoURL: imgUrl
+      })
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
